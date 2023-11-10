55ef2c2f13bae55a8a704d12f7d6df326d374c84
Moses Bartena Towet <121856163+4ourpixels@users.noreply.github.com>
2023-11-10 21:08:09 +0300

    Add files via upload

--- website/views.py
+++ website/views.py
@@ -11,13 +11,13 @@ from django.conf import settings
 def index(request):
     page_name = "- Masters of the Digital Realm"
     context = {
-        'page_name', page_name
+        'page_name': page_name,
     }
     return render(request, 'index.html', context)
 
 def about(request):
     page_name = "- About Us"
     context = {
-        'page_name', page_name
+        'page_name': page_name,
     }
     return render(request, 'about.html', context)
