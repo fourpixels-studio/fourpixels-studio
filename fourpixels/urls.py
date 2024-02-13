60c27072ac0f4daaf39f8e815091947a669a47d5
Four Pixels <121856163+4ourpixels@users.noreply.github.com>
2024-02-13 12:20:46 +0300

    Added custom error 404 & 500 pages

--- fourpixels/urls.py
+++ fourpixels/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import settings
+from .import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500
+from website.views import error_404, error_500
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,6 +14,9 @@ urlpatterns = [
 admin.site.site_header = "Four Pixels Admin"
 
 # Tab/Site Title
-admin.site.site_header = "Four Pixels - Admin"
+admin.site.site_header = "Four Pixels"
 
 admin.site.index_title = "Four Pixels - Admin"
+
+handler404 = error_404
+handler500 = error_500
