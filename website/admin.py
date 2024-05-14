16daf1a1cf0f332df645f77c517bdcc4d77ccac5
Four Pixels <121856163+4ourpixels@users.noreply.github.com>
2024-05-14 22:01:08 +0300

    Added Accordion, Service and Homepage

--- website/admin.py
+++ website/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+
 admin.site.register(Blog)
 admin.site.register(Newsletter)
 admin.site.register(Customer)
 admin.site.register(AboutSection)
-admin.site.register(MessageTag)
 admin.site.register(Contact)
 admin.site.register(Testimonial)
 admin.site.register(Merchandise)
 admin.site.register(ClientPortoflio)
+admin.site.register(HomePage)
+admin.site.register(Service)
+admin.site.register(Accordion)
