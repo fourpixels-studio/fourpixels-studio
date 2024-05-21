118be27d05407a2be62eed0c875fb2faf045b6fe
Four Pixels <121856163+4ourpixels@users.noreply.github.com>
2024-05-21 23:32:13 +0300

    First commit

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarToggler = document.querySelector(".navbar-toggler");
+    var navbar = document.querySelector(".navbar");
+
+    navbarToggler.addEventListener("click", function() {
+        navbar.classList.toggle("bg-white");
+    });
+});
