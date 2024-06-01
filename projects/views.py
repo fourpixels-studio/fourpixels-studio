63d38c80b2716711beded83a2c69fa851101d726
Four Pixels <121856163+4ourpixels@users.noreply.github.com>
2024-06-01 15:44:03 +0300

    Added thumbnail

--- projects/views.py
+++ projects/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Project
 from website.get_items import get_projects
 from blogs.utils import update_views
+from seo_management.models import SEO
+seo = SEO.objects.first()
 
 
 def projects_list(request):
     context = {
         'title_tag': "Projects",
         'projects': get_projects(),
+        'meta_thumbnail': seo.meta_thumbnail.url,
     }
     return render(request, 'projects_list.html', context)
 
@@ -17,7 +20,7 @@ def project_detail(request, slug):
     context = {
         'project': project,
         'title_tag': project.name,
-        'meta_thumbnail': project.logo.url,
+        'meta_thumbnail': seo.meta_thumbnail.url,
         'meta_keywords': project.category,
         'meta_description': project.description,
         'projects': get_projects(),
