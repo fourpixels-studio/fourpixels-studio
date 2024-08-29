2c7d7b533e4610b3ed9336ed7daa226b77ecd7a0
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-29 16:36:47 +0300

    Added ProjectMedia and updated Project media property

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,7 @@ from django.utils.text import slugify
 from django.urls import reverse
 from hitcount.models import HitCount
 from django.contrib.contenttypes.fields import GenericRelation
+from django.utils.html import mark_safe
 
 
 class Category(models.Model):
@@ -24,7 +25,10 @@ class Project(models.Model):
     link_name = models.CharField(max_length=40, blank=True, null=True)
     blog_link = models.TextField(blank=True, null=True)
     app_link = models.TextField(blank=True, null=True)
-    logo = models.ImageField(blank=True, null=True)
+    logo = models.ImageField(
+        upload_to="projects/logos/", blank=True, null=True)
+    highlight = models.BooleanField(default=False, null=True, blank=True)
+    image_placeholder = models.TextField(blank=True, null=True)
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, null=True, blank=True)
     slug = models.SlugField(unique=True, null=True, blank=True)
@@ -56,3 +60,39 @@ class Project(models.Model):
         if self.hit_count_generic.exists():
             return self.hit_count_generic.first().hits
         return 0
+
+    @property
+    def get_project_media(self):
+        project_media_list = ProjectMedia.objects.filter(project=self)
+        media_list = []
+        for project_media in project_media_list:
+            media_element = None
+            if project_media.video:
+                media_element = mark_safe(
+                    f'<video controls class="img-fluid rounded-4 w-100"><source src="{project_media.video.url}" type="video/mp4"></video>')
+            elif project_media.image:
+                media_element = mark_safe(
+                    f'<img src="{project_media.image.url}" class="img-fluid rounded-4 border w-100"/>')
+            media_list.append(
+                (
+                    project_media.pk, media_element,
+                    project_media.name, project_media.description
+                )
+            )
+        return media_list
+
+
+class ProjectMedia(models.Model):
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    video = models.FileField(
+        blank=True, null=True,
+        upload_to="projects/media/")
+    image = models.FileField(
+        blank=True, null=True,
+        upload_to="projects/media/")
+
+    def __str__(self):
+        return f"{self.name} - {self.project.name}"
