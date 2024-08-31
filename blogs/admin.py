c127ca000a77b81747b5a240f731eb2a39c677ac
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-31 17:25:48 +0300

    Added django-summernote

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
+from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
 from .models import Blog
-admin.site.register(Blog)
+
+
+class BlogAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+
+admin.site.register(Blog, BlogAdmin)
