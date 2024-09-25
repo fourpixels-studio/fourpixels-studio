7824397a805ba2441c3802cee8bcce6e1e6c27f2
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-25 23:41:22 +0300

    Imported apps form individual files

--- applications/urls.py
+++ applications/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import applications_list, image_search, image_compression
-
+from .views import applications_list
+from .image_search import image_search
+from .image_compression import image_compression
+from .bloomgerg_news_finder import bloomgerg_news_finder
 
 urlpatterns = [
     path("", applications_list, name="applications_list"),
     path("image-compression/", image_compression, name="image_compression"),
     path("image-search/", image_search, name="image_search"),
+    path("Bloomberg-News-Finder/", bloomgerg_news_finder, name="bloomgerg_news_finder"),
 ]
