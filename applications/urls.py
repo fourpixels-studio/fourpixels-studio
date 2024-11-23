4c3c62e5f26de614ee40cf71c31cd1ff43d8b3b6
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-11-23 11:48:04 +0300

    Added QR Code Generator

--- applications/urls.py
+++ applications/urls.py
@@ -3,10 +3,14 @@ from .views import applications_list
 from .image_search import image_search
 from .image_compression import image_compression
 from .bloomgerg_news_finder import bloomgerg_news_finder
+from .qr_code_generator import qr_code_generator, download_qr_code
+
 
 urlpatterns = [
     path("", applications_list, name="applications_list"),
     path("image-compression/", image_compression, name="image_compression"),
     path("image-search/", image_search, name="image_search"),
     path("Bloomberg-News-Finder/", bloomgerg_news_finder, name="bloomgerg_news_finder"),
+    path("QR-Code-Generator/", qr_code_generator, name="qr_code_generator"),
+    path("download/QR-Code-Generator/", download_qr_code, name="download_qr_code"),
 ]
