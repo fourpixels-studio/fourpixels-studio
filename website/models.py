db09637db252b13279e03bcfc1c2c7e9c96885a3
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-07 13:09:45 +0300

    Added subject in the Contact model

--- website/models.py
+++ website/models.py
@@ -27,13 +27,14 @@ class Testimonial(models.Model):
 
 class Contact(models.Model):
     name = models.CharField(max_length=120, null=True, blank=True)
-    message = models.TextField(null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
+    subject = models.CharField(max_length=200, blank=True, null=True)
+    message = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
-        return f'{self.name} | {self.message[:100]} | {self.date}'
+        return f'{self.name} | {self.subject} | {self.date}'
 
 
 class HomePage(models.Model):
