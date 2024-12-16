d317a262f96325b5796b4f4c7b81fe9f37c7d2fe
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-16 18:50:47 +0300

    Added get_share_link and the imported the settings

--- blogs/models.py
+++ blogs/models.py
@@ -1,32 +1,32 @@
 from django.db import models
-from django.utils.text import slugify
 from django.urls import reverse
-from hitcount.models import HitCount
-from django.contrib.contenttypes.fields import GenericRelation
-from django.urls import reverse
-from django_resized import ResizedImageField
+from django.conf import settings
 from django.utils import timezone
+from hitcount.models import HitCount
+from django.utils.text import slugify
+from django_resized import ResizedImageField
+from django.contrib.contenttypes.fields import GenericRelation
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=200, blank=True, null=True)
     introduction = models.TextField(blank=True, null=True)
     content = models.TextField(blank=True, null=True)
+    paragraph_1 = models.TextField(blank=True, null=True)
+    paragraph_2 = models.TextField(blank=True, null=True)
+    paragraph_3 = models.TextField(blank=True, null=True)
     conclusion = models.TextField(blank=True, null=True)
     tags = models.TextField(blank=True, null=True)
     author = models.CharField(max_length=50, blank=True, null=True)
     category = models.CharField(max_length=50, blank=True, null=True)
     cover = models.ImageField(blank=True, null=True)
-    pub_date = models.DateTimeField(
-        default=timezone.now, blank=True, null=True)
+    pub_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
     summary = models.TextField(blank=True, null=True)
     meta_description = models.TextField(blank=True, null=True)
     meta_keywords = models.TextField(blank=True, null=True)
     slug = models.SlugField(unique=True, null=True, blank=True)
-    hit_count_generic = GenericRelation(
-        HitCount, object_id_field='object_pk', related_query_name='hit_count_generic_relation')
-    thumbnail = ResizedImageField(
-        size=[1024, 600], upload_to="blogs/", null=True, blank=True, force_format='JPEG', quality=90,)
+    hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk', related_query_name='hit_count_generic_relation')
+    thumbnail = ResizedImageField(size=[1200, 630], crop=['middle', 'center'], quality=75, upload_to='thumbnails/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} - Published On: {self.pub_date.strftime('%a, %b %d, %Y')}"
@@ -50,3 +50,9 @@ class Blog(models.Model):
         if self.hit_count_generic.exists():
             return self.hit_count_generic.first().hits
         return 0
+
+    @property
+    def get_share_link(self):
+        current_site = settings.MY_SITE
+        blog_url = self.get_url
+        return f"{current_site}{blog_url}"
