687715f3b9847ccb88e2bb88146f4e6d273da15e
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-05-23 16:50:48 +0300

    Added: ProjectMedia and Summernote in Project

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Category, Project
+from .models import Category, Project, ProjectMedia
+from django_summernote.admin import SummernoteModelAdmin
 
 admin.site.register(Category)
-admin.site.register(Project)
+admin.site.register(ProjectMedia)
+
+class ProjectAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+admin.site.register(Project, ProjectAdmin)
