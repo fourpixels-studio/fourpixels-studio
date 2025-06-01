1f633c68078d1612598efc4c3bfbbec6d6d9775b
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-06-01 14:46:39 +0300

    Added: sitemap and safe deploy settings

--- fourpixels/settings.py
+++ fourpixels/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
     'django_resized',
     'hitcount',
     'website',
@@ -212,3 +214,13 @@ LOGGING = {
         },
     },
 }
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
