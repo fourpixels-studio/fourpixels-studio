327c69a6f39251a034f544e24da70b3caa5bce57
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-06-05 13:48:09 +0300

    Added: projects_list

--- website/sitemaps.py
+++ website/sitemaps.py
@@ -8,7 +8,7 @@ class StaticSitemap(Sitemap):
     def items(self):
         return [
             'applications_list', 'image_compression', 'image_search',
-            'bloomberg_news_finder', 'qr_code_generator', 'index', 'about',
+            'bloomberg_news_finder', 'qr_code_generator', 'index', 'about', 'projects_list',
         ]
 
     def location(self, item):
