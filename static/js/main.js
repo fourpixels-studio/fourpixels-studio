ab568f2d581cc7d0f6e4983dd52f5f0604910ad5
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-09-08 19:22:45 +0300

    Added: aos

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,14 @@ function copyToClipboard() {
     document.execCommand("copy");
     copyBtn.innerHTML = "Link Copied";
 }
+function aos_init() {
+    AOS.init({
+        duration: 800,
+        easing: 'slide',
+        once: true,
+        mirror: false
+    });
+}
+window.addEventListener('load', () => {
+    aos_init();
+});
