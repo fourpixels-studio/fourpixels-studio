11f015fb5f5a9011266e3877403a5728bc6adb57
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-09-13 21:54:24 +0300

    Update: added contact

--- website/sitemaps.py
+++ website/sitemaps.py
@@ -7,7 +7,7 @@ from django.contrib.sitemaps import Sitemap
 class StaticSitemap(Sitemap):
     def items(self):
         return [
-            'applications_list', 'image_compression', 'image_search',
+            'applications_list', 'image_compression', 'image_search', 'contact',
             'bloomberg_news_finder', 'qr_code_generator', 'index', 'about', 'projects_list',
         ]
 
