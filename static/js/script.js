742b73505ff8c5fe4ccfe0c9c2471d2779ae3499
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2026-02-20 14:58:57 +0300

    Delete static directory

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var navbarToggler = document.querySelector(".navbar-toggler");
-    var navbar = document.querySelector(".navbar");
-
-    navbarToggler.addEventListener("click", function() {
-        navbar.classList.toggle("bg-white");
-    });
-});
