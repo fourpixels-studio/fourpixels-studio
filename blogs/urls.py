1265d11b5bbddf5ce0f9b6bcdbe0df473f2f58f2
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2026-02-20 14:56:50 +0300

    Delete blogs directory

--- blogs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import blog_detail, blog_list
-
-
-urlpatterns = [
-    path("", blog_list, name="blog_list"),
-    path("<slug:slug>/", blog_detail, name="blog_detail"),
-]
