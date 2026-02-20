ed57723590d4df0574e5f7c5293de1a558969d81
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2026-02-20 14:58:36 +0300

    Delete projects directory

--- projects/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import projects_list, project_detail, update_project
-
-
-urlpatterns = [
-    path("", projects_list, name="projects_list"),
-    path("<slug:slug>/", project_detail, name="project_detail"),
-    path("update/<int:pk>/", update_project, name="update_project"),
-]
