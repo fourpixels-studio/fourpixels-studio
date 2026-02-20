c76de9f613286290b86aecf7f27e8d3e0b721480
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2026-02-20 14:59:10 +0300

    Delete website directory

--- website/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-admin.site.register(Newsletter)
-admin.site.register(Contact)
-admin.site.register(Testimonial)
-admin.site.register(HomePage)
-admin.site.register(Service)
-admin.site.register(Accordion)
-admin.site.register(About)
