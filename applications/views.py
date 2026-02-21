6b9c199f4446959dc12b432ecf2017e50275ccf2
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2026-02-21 17:54:10 +0300

    Update applications_list to use Application model
    Removed blog cover images and added applications context.

--- applications/views.py
+++ applications/views.py
@@ -1,11 +1,10 @@
-from blogs.models import Blog
+from .models import Application
 from django.shortcuts import render
 
 
 def applications_list(request):
     context = {
         "title_tag": "Applications",
-        'image_search_cover': Blog.objects.get(pk=4).cover,
-        'image_compression_cover': Blog.objects.get(pk=3).cover,
+        "applications": Application.objects.all(),
     }
     return render(request, 'applications_list.html', context)
