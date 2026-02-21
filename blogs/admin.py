ef10c9d12944f1dd4759c667712e974ef4f3190d
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2026-02-21 17:45:33 +0300

    Register Category model in admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,10 +1,11 @@
-from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Category
+from django_summernote.admin import SummernoteModelAdmin
 
 
 class BlogAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 
+admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
